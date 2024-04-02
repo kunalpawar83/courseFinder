41ad2d348fa6c1842f5c9d137a3fb1f638206640
kunalpawar83 <kunalpawar8319@gmail.com>
2024-04-02 10:30:34 +0530

    init commit

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console.log("kunal pawar")
+const express = require('express');
+const bodyparser = require('body-parser');
+require('dotenv').config();
+
+
+const User = require('./models/userModel.js');
+
+
+const app = express();
+app.use(bodyparser.json());
+const PORT =  process.env.PORT || 3000;
+
+app.get('/api/v1/user',(req,res)=>{
+    const userdata =  User.find();
+    res.json(userdata);
+})
+
+app.post('/api/v1/user',(req,res)=>{
+    const data = User.create(req.body);
+
+    res.status(201).json({
+        Message:"success"
+    })
+})
+
+app.listen(PORT ,()=>{
+    console.log('listening on port'+PORT);
+})
+
