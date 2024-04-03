ebe6050a1a00fd7cabc8d814db65ed63a983598c
kunalpawar83 <kunalpawar8319@gmail.com>
2024-04-03 15:57:15 +0530

    crud operation with superadmin

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const Course = require('../models/couseModel.js');
 const User = require('../models/userModel.js');
+const {jwtAuthMiddleware,generateToken} = require('../jwt.js');
 
 const router = express.Router();
+
+// GET  ROUTES TO GET DATA
 router.get('/',async(req,res)=>{
     try{
       const data = await Course.find();
@@ -20,12 +23,109 @@ router.get('/',async(req,res)=>{
     }
 })
 
-router.post('/:id', async(req,res)=>{
-     const userData = await User.findById(req.params.id);
-      console.log(userData.role);
-     res.status(201).json({
-         data:userData
+// POST ROUTES TO CREATE DATA
+router.post('/',jwtAuthMiddleware, async(req,res)=>{
+
+    // get user data 
+  const userData = await User.findById(req.user.id);
+      if(userData.role!='superAdmin'){
+       return res.status(400).json({
+          status:"fail",
+          error:"you do not perform this operation"
+       })
+          }
+  try{
+              
+     // create course data 
+    const data = req.body ;
+    const newCourse = new Course(data);
+    const response = await newCourse.save();
+    console.log('data saved');
+    res.status(201).json({
+      status:"success",
+      response
+    });  
+
+
+  }catch(err){
+      console.error(err);
+      res.status(500).json({
+      error:"internal server Error"
+      })
+     }
+})
+
+// PUT ROUTES TO UPDATE DATA
+router.put('/:id',jwtAuthMiddleware, async(req,res)=>{
+
+  // get user data 
+  const userData = await User.findById(req.user.id);
+    if(userData.role!='superAdmin'){
+     return res.status(400).json({
+        status:"fail",
+        error:"you do not perform this operation"
      })
+        }
+  try{
+            
+      const courseId  = req.params.id;
+       const courseData = req.body;
+       
+       const response = await Course.findByIdAndUpdate(courseId,courseData,{
+         new:true,
+         runValidators:true
+       })
+
+       if(!response){
+        return res.status(404).json({
+            status:"fail",
+            error:"Course not found"
+        })
+       }
+
+       console.log('data updated');
+       res.status(200).json({
+        status:"success",
+        data:response
+    })
+
+  }catch(err){
+    console.error(err);
+    res.status(500).json({
+    error:"internal server Error"
+    })
+   }
+})
+
+
+// DETELE ROUTES TO DETELE THE DATA
+router.delete('/:id',jwtAuthMiddleware, async(req,res)=>{
+
+  // get user data 
+const userData = await User.findById(req.user.id);
+    if(userData.role!='superAdmin'){
+     return res.status(400).json({
+        status:"fail",
+        error:"you do not perform this operation"
+     })
+        }
+try{
+            
+   // create course data 
+   const courseId = req.params.id;
+    await Course.findByIdAndDelete(courseId)
+  console.log('data saved');
+  res.status(201).json({
+    status:"success",
+  });  
+
+
+}catch(err){
+    console.error(err);
+    res.status(500).json({
+    error:"internal server Error"
+    })
+   }
 })
 
 module.exports =router;
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const {jwtAuthMiddleware,generateToken} = require('./jwt.js');
-
 
 const app = express();
 const db  = require('./db.js');
@@ -13,7 +11,7 @@ app.use(bodyParser.json())
 
 // ROUTES 
 app.use('/user',require('./routes/userRoute.js'));
-app.use('/course',jwtAuthMiddleware,require('./routes/courseRoute.js'));
+app.use('/course',require('./routes/courseRoute.js'));
 
 app.listen(process.env.PORT,()=>{
     console.log("listening port on 3000");
